dd0a1bbbfe1d344a48db5c4449946a55ffb173e1
xxxxyd7 <cyeond@gmail.com>
2023-07-18 00:55:46 +0900

    이미지 선택 구현(PHPickerViewController)

--- LiveActivitySample/LiveActivitySample/ViewController.swift
+++ LiveActivitySample/LiveActivitySample/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var phPicker: PHPickerViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.titleTF.delegate = self
+        self.configurePHPicker()
+        
+    }
+    
+    private func configurePHPicker() {
+        var pickerConfig = PHPickerConfiguration()
+        pickerConfig.filter = .images
+        pickerConfig.selectionLimit = 1
+        
+        self.phPicker = PHPickerViewController(configuration: pickerConfig)
+        self.phPicker?.delegate = self
     }
     
     @IBAction func start(_ sender: UIButton) {
@@ -33,8 +48,18 @@ class ViewController: UIViewController {
             LiveActivityManager.shared.updateActivity(title: title, time: Date() + 60.0)
         }
     }
+    
+    @IBAction func changeImage(_ sender: UIButton) {
+        guard let phPicker = self.phPicker else {
+            //
+            return
+        }
+        
+        self.present(phPicker, animated: true)
+    }
 }
 
+// MARK: UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -42,3 +67,35 @@ extension ViewController: UITextFieldDelegate {
     }
 }
 
+// MARK: PHPickerViewControllerDelegate
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        picker.dismiss(animated: true)
+        
+        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                guard let image = image as? UIImage else {
+                    //
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.imageView.image = image.resize(newWidth: 100.0)
+                }
+            }
+        } else {
+            //
+        }
+    }
+}
+
+extension UIImage {
+    func resize(newWidth: CGFloat) -> UIImage {
+        let size = CGSize(width: newWidth, height: newWidth)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { context in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        return renderImage
+    }
+}
